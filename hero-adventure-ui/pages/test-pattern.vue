5328c613c94f5a1cf096ba3ceae44df719ec639d
Zanzibar <zanzisworld42@gmail.com>
2025-01-09 19:02:31 -0800

    Adjust test-pattern

--- hero-adventure-ui/pages/test-pattern.vue
+++ hero-adventure-ui/pages/test-pattern.vue
@@ -2,16 +2,14 @@
   <div class="w-5/6 min-w-96">
     <h1>Test Pattern - <em>H1 Orbitron</em></h1>
     <h1 class="font-heading-serif">How About <em>Chonburi?</em></h1>
-    <h1 class="font-heading-slab">
-      Or What About <em>Roboto Slab?</em>
-    </h1>
+    <h1 class="font-heading-slab">Or What About <em>Roboto Slab?</em></h1>
     <p class="font-body">
       (roboto) This is where we test the <em>default styles</em> and layout.
     </p>
     <p class="font-heading-serif">
       (chonburi) This is where we test the <em>default styles</em> and layout.
     </p>
-    <p class="font-heading-sans-serif">
+    <p class="font-heading-slab">
       (roboto slab) This is where we test the <em>default styles</em> and
       layout.
     </p>
@@ -26,32 +24,43 @@
       <li>Second, you must...</li>
       <li>Finally, why not...</li>
     </ul>
-    <ol>
+    <ol class="font-body-serif">
       <li>First of all...</li>
       <li>Second, you must...</li>
       <li>Finally, why not...</li>
     </ol>
-    <ol class="font-cursive">
+    <ol class="font-cursive text-sm">
       <li>These are days of power.</li>
       <li>These are days of intrigue.</li>
       <li>This is the golden age.</li>
     </ol>
-    <p class="my-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue ac sem quis aliquam. Nulla
-      tempus neque
-      urna, sed sollicitudin felis euismod ac. Vestibulum nisi lorem, auctor eu vehicula euismod, faucibus at risus. In
-      hac habitasse platea dictumst. Suspendisse blandit massa ut sapien aliquet porta in non massa. Cras feugiat
-      suscipit turpis, vitae pretium purus efficitur at. Donec maximus aliquam orci at pulvinar.</p>
-    <p class="font-body-serif text-sm">Morbi suscipit massa ac
-      faucibus molestie. Proin cursus lectus in nisl ultricies malesuada. Phasellus eget nunc suscipit, tempus ante sed,
-      rutrum purus. Etiam tincidunt metus luctus velit eleifend, a blandit est pellentesque. Curabitur varius malesuada
-      semper.</p>
+    <p class="my-4">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue ac
+      sem quis aliquam. Nulla tempus
+      <em>neque urna, sed sollicitudin felis euismod ac. Vestibulum nisi lorem,
+        auctor eu vehicula euismod, faucibus at risus. In</em>
+      hac habitasse platea dictumst. Suspendisse blandit massa ut sapien aliquet
+      porta in non massa. Cras feugiat suscipit turpis, vitae pretium purus
+      efficitur at. Donec maximus aliquam orci at pulvinar.
+    </p>
+    <p class="font-body-serif">
+      Morbi suscipit massa ac faucibus molestie. Proin cursus lectus in nisl
+      ultricies malesuada. Phasellus eget nunc suscipit, tempus
+      <em>ante sed, rutrum purus. Etiam tincidunt metus luctus</em> velit
+      eleifend, a blandit est pellentesque. Curabitur varius malesuada semper.
+    </p>
     <p class="font-cursive my-4">
-      Donec commodo convallis ex ut gravida. Vivamus varius auctor elit. Maecenas imperdiet, eros vitae euismod
-      ultrices, magna nulla blandit velit, sit amet fringilla lorem nulla at enim. Quisque nec eleifend neque. Proin sit
-      amet mi risus. Mauris sodales, lorem et consectetur semper, neque tellus semper felis, vitae tincidunt nisl mauris
-      ut libero. Morbi eget justo scelerisque, luctus ipsum id, ultricies ligula. Pellentesque habitant morbi tristique
-      senectus et netus et malesuada fames ac turpis egestas. Integer risus diam, pulvinar eu euismod eget, tincidunt id
-      neque. Donec rutrum ac lacus varius laoreet. Nulla sed iaculis mauris, vitae mollis purus.</p>
+      Donec commodo convallis ex ut gravida. Vivamus varius auctor elit.
+      Maecenas imperdiet, eros vitae euismod ultrices, magna nulla blandit
+      velit, sit amet fringilla lorem nulla at enim. Quisque nec eleifend neque.
+      Proin sit amet mi risus. Mauris sodales, lorem et consectetur semper,
+      neque tellus semper felis, vitae tincidunt nisl mauris ut libero. Morbi
+      eget justo scelerisque, luctus ipsum id, ultricies ligula. Pellentesque
+      habitant morbi tristique senectus et netus et malesuada fames ac turpis
+      egestas. Integer risus diam, pulvinar eu euismod eget, tincidunt id neque.
+      Donec rutrum ac lacus varius laoreet. Nulla sed iaculis mauris, vitae
+      mollis purus.
+    </p>
   </div>
 </template>
 
